72eca18deb6a09add1ff67a3f66f5d807ddf3112
halesyeah3 <seanhales@ymail.com>
2020-05-16 19:08:22 -0400

    cleaned up lotto number generation

--- lotto.py
+++ lotto.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 from random import randint
+import random
 
 print('Welcome to the Lotto Game')
 
@@ -16,35 +17,30 @@ lottoNumbers = input('Enter your 5 lotto numbers (1-60) seperated by a comma')
 lNumbers = lottoNumbers.split(",")
 lNumber = [int(i) for i in lNumbers]
 
-print('Your Numbers:', lNumber)
-
-#There has to be a better way to generate 5 random numbers with the same min/max
-#Currently these numbers can duplicate but should be unique
-num1 = randint(1,60) #1
-num2 = randint(1,60) #2
-num3 = randint(1,60) #3
-num4 = randint(1,60) #4
-num5 = randint(1,60) #5
-
+if all(i <= 60 for i in lNumber):
+    print('Your Numbers:', lNumber)
 #Clean this up
-lotto = [num1, num2, num3, num4, num5]
-lottoWinners = str(lotto)
-print('Winning Numbers: ', lottoWinners)
+    lotto = random.sample(range(1, 60), 5)
+    print('Winning Numbers: ', lotto)
 
 
 #Count the number of matching numbers from user guess and lotto winners
-correctNum = sum(x == y for x, y in zip(lNumber, lotto))
+    correctNum = sum(x == y for x, y in zip(lNumber, lotto))
 
 
         
 #Notify the user of their result
-print('You got', correctNum, 'correct')
+    print('You got', correctNum, 'correct')
 
-if correctNum == 5:
-    print('Congratulations! You won $100,000')
-elif correctNum == 4:
-    print('Congratulations! You won $4,000')
-elif correctNum == 3:
-    print('Congratulations! You won $5')
+    if correctNum == 5:
+        print('Congratulations! You won $100,000')
+    elif correctNum == 4:
+        print('Congratulations! You won $4,000')
+    elif correctNum == 3:
+        print('Congratulations! You won $5')
+    else:
+        print('Play Again')
+        
 else:
-    print('Play Again')
+    lottoNumbers = input('Enter your 5 lotto numbers (1-60) seperated by a comma')
+#Return to top instead?
