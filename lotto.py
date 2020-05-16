4d9242aa8c3307c232fb05b75170a1e88ebfc3ca
halesyeah3 <seanhales@ymail.com>
2020-05-16 14:10:19 -0400

    Lotto Game

---
+++ lotto.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+from random import randint
+
+print('Welcome to the Lotto Game')
+
+lottoNumbers = input('Enter your 5 lotto numbers (1-60) seperated by a comma')
+lNumbers = lottoNumbers.split(",")
+lNumber = [int(i) for i in lNumbers]
+
+print(lNumber)
+
+#There has to be a better way to generate 5 random numbers with the same min/max
+num1 = 1 #randint(1,60)
+num2 = 2 #randint(1,60)
+num3 = 3 #randint(1,60)
+num4 = 4 #randint(1,60)
+num5 = 5 #randint(1,60)
+
+lotto = [num1, num2, num3, num4, num5]
+lottoWinners = str(lotto)
+print(lottoWinners)
+
+
+#Count the number of matching numbers from user guess and lotto winners
+correctNum = sum(x == y for x, y in zip(lNumber, lotto))
+
+
+        
+#Notify the user of their result
+print('You got', correctNum, 'correct')
