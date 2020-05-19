1ddf83a03d22416e31f01867b8883503789e38bc
halesyeah3 <seanhales@ymail.com>
2020-05-19 13:35:20 -0400

    updated validation

--- lotto.py
+++ lotto.py
@@ -1,4 +1,6 @@
+  GNU nano 3.2                                            lotto.py                                                      
 #!/bin/python3
+
 from random import randint
 import random
 
@@ -8,54 +10,52 @@ def checkIfDuplicates_1(listOfElems):
         return False
     else:
         return True
-    
+
 def checkCorrect(int):
     if int == 5:
-        print('Congratulations! You won $100,000')    
+        print('Congratulations! You won $100,000')
     elif int == 4:
-        print('Congratulations! You won $4,000')    
+        print('Congratulations! You won $4,000')
     elif int == 3:
-        print('Congratulations! You won $5')    
+        print('Congratulations! You won $5')
     else:
-        print('Play Again')   
+        print('Play Again')
         print('Welcome to the Lotto Game')
-        
+
 while True:
     lottoNumbers = input('Enter your 5 lotto numbers (1-60) seperated by a comma')
-#maybe int(lottoNumbers) after stripping commas from the list to validate numbers
     lNumbers = lottoNumbers.split(",")
-#This isn't working
     try:
-        lNumber = [int(i) for i in lNumbers]
-        if all(i > 60 for i in lNumber) or all(i < 0 for i in lNumber):
-            continue
-        else:
-            result = checkIfDuplicates_1(lNumbers)
+        lNumber = (int(i) for i in lNumbers)
+        #Why does only one condition work?
+        if all(i <= 60 for i in lNumber) and all(i >= 1 for i in lNumber):
+            
+#Should only draw lotto numbers if validation is met
+            lotto = random.sample(range(1, 60), 5)
+            print('Your Numbers: ', lNumbers)
+            print('Winning Numbers: ', lotto)
+        #Count the number of matching numbers from user guess and lotto winners
+            correctNum = sum(x == y for x, y in zip(lNumber, lotto))
+            print('You got', correctNum, 'correct')
 
-        
-        if result:
-            continue
+            if correctNum == 5:
+                print('Congratulations! You won $100,000')
+            elif correctNum == 4:
+                print('Congratulations! You won $4,000')
+            elif correctNum == 3:
+                print('Congratulations! You won $5')
+            else:
+                print('Thanks for Playing!')
+            #Loop Back after input from user
+            #playagain = input('Play Again? Y/N')
+            #if playagain == 'Y' or playagain == 'y':
+            #    continue
+            #else:
+            #    print('Thanks For Playing!')
+    
         else:
-#Clean this up
-                lotto = 1, 2, 3, 4, 5 #random.sample(range(1, 60), 5)
-                print('Winning Numbers: ', lotto)
-
-#Count the number of matching numbers from user guess and lotto winners
-                
-        
-#Notify the user of their result
-                correctNum = sum(x == y for x, y in zip(lNumber, lotto))
-                print('Your Numbers:', lNumber)
-                print('You got', correctNum, 'correct')
-                checkCorrect(correctNum)
-#Determine if Winner
+            continue
     except ValueError:
         continue
-
+        
     
-
-            
-        
-        
-#lottoNumbers = input('Enter your 5 lotto numbers (1-60) seperated by a comma')
-#Return to top instead?
